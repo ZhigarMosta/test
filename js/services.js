fc4b91e1fda6a35749ec9228ca10d451717c791d
ZhigarMosta <118725082+ZhigarMosta@users.noreply.github.com>
2024-04-03 11:07:13 +0500

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 async function fetchData() {
   try {
-    const response = await fetch("./../json/services.json");
+    const response = await fetch("https://raw.githubusercontent.com/ZhigarMosta/test/main/json/services.json");
     return await response.json();
   } catch (error) {
     console.error("Ошибка загрузки файла:", error);
